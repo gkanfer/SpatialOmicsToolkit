0e91600978b6e0efd8918042c0e00bed71f7e9a1
“Fulan <gkanfer007@gmail.com>
2024-07-21 15:34:05 -0400

    xenium_read

--- raed_write_xinum_image.py
+++ raed_write_xinum_image.py
@@ -1,41 +1,25 @@
-# Import Python libraries
-# This code uses python v3.12.0, tifffile v2023.9.26, matplotlib v3.8.2
 import tifffile
 import matplotlib.pyplot as plt
 import os
+import seaborn as sns
+from skimage.transform import resize
+import argparse
+import numpy as np
 
-path_morphology_focus ="/data/kanferg/Sptial_Omics/playGround/Data/Xenium/Xenium_Prime_Human_Lymph_Node_Reactive_FFPE/morphology_focus"
-file_name = "morphology_focus_0000.ome.tif"
-file_path = os.path.join(path_morphology_focus, file_name)
+def raed_write_xinum_image(file_name,path_morphology_focus,scale,ch,prefix):
+    file_path = os.path.join(path_morphology_focus, file_name)
+    fullres_multich_img = tifffile.imread(file_path, is_ome=True, level=0, aszarr=False)
+    fullres_multich_img_ch = fullres_multich_img[ch,:,:]
+    image_resized = resize(fullres_multich_img_ch, (fullres_multich_img_ch.shape[0] // scale, fullres_multich_img_ch.shape[1] // scale), anti_aliasing=True)
+    image_scaled = ((image_resized /image_resized.max())  * 255).astype(np.uint8)
+    skimage.io.imsave(os.path.join(path_morphology_focus, f'_mean_{prefix}' + '.png'), image_scaled)
 
-# Option 1: Load full resolution image channels
-# The following may produce a warning: 'OME series cannot read multi-file pyramids'. This is because tifffile does not support loading a pyramidal multi-file OME-TIFF file. Only the full resolution (level=0) data will load for all channels in the directory.
-fullres_multich_img = tifffile.imread(file_path, is_ome=True, level=0, aszarr=False)
-
-# Examine shape of array (number of channels, height, width), e.g. (4, 40867, 31318)
-fullres_multich_img.shape
-
-# Extract number of channels, e.g. 4
-n_ch = fullres_multich_img.shape[0]
-
-# Plot each channel
-fig, axes = plt.subplots(ncols=n_ch, nrows=1, squeeze=False)
-for i in range(n_ch):
-    axes[0, i].imshow(fullres_multich_img[i], cmap="gray")
-    axes[0, i].set_title(f"Channel: {i}")
-plt.savefig(os.path.join(path_morphology_focus,'tifffile_fullres_four_channels.png'))
-
-# Option 2: Load a single channel image at any resolution, e.g., level=0 or level=1. Note 'is_ome' is set to False.
-# Load one of the multi-file OME-TIFF files as a regular TIFF file at full resolution.
-fullres_img_tiff = tifffile.imread(file_path, is_ome=False, level=0)
-
-# Now load the file at downsampled resolution
-downsampled_img = tifffile.imread(file_path,is_ome=False, level=1)
-
-# Plot the full resolution and downsampled images side-by-side
-fig, axes = plt.subplots(ncols=2, nrows=1, squeeze=False)
-axes[0, 0].imshow(fullres_img_tiff, cmap="gray")
-axes[0, 0].set_title(f"Full resolution: {fullres_img_tiff.shape}")
-axes[0, 1].imshow(downsampled_img, cmap="gray")
-axes[0, 1].set_title(f"Downsampled: {downsampled_img.shape}")
-plt.savefig(os.path.join(path_morphology_focus,'example_fullres_downsample.png'))
+if (__name__ == "__main__"):
+    parser = argparse.ArgumentParser(description='sbatch create')
+    parser.add_argument('--file_name', dest='file_name', type=str, required=True)
+    parser.add_argument('--path_morphology_focus', dest='path_morphology_focus', type=str, required=True)
+    parser.add_argument('--scale', dest='scale', type=int, required=True)
+    parser.add_argument('--ch', dest='ch', type=int, required=True)
+    parser.add_argument('--prefix', dest='prefix', type=str, required=True)
+    args = parser.parse_args()
+    raed_write_xinum_image(file_name = args.file_name,path_morphology_focus = args.path_morphology_focus,scale = args.scale, ch = args.ch, prefix = args.prefix)
--- raed_write_xinum_image.sh
+++ raed_write_xinum_image.sh
@@ -7,5 +7,42 @@
 
 source myconda 
 mamba activate squidpy-voyagerpy 
-python raed_write_xinum_image.py
 
+file_name="morphology_focus_0000.ome.tif"
+path_morphology_focus="/data/kanferg/Sptial_Omics/playGround/Data/Xenium/Xenium_Prime_Human_Lymph_Node_Reactive_FFPE/morphology_focus"
+scale=10
+channels=(0 1 2 3)
+prefix="morphology_focus_0000"
+
+for ch in "${channels[@]}"
+do
+    pref="${prefix}_${ch}"
+    python raed_write_xinum_image.py --file_name "$file_name" --path_morphology_focus "$path_morphology_focus" --scale "$scale"  --ch "$ch" --prefix "$pref"
+done
+
+file_name="morphology_focus_0001.ome.tif"
+prefix="morphology_focus_0001"
+
+for ch in "${channels[@]}"
+do
+    pref="${prefix}_${ch}"
+    python raed_write_xinum_image.py --file_name "$file_name" --path_morphology_focus "$path_morphology_focus" --scale "$scale"  --ch "$ch" --prefix "$pref"
+done
+
+file_name="morphology_focus_0002.ome.tif"
+prefix="morphology_focus_0002"
+
+for ch in "${channels[@]}"
+do
+    pref="${prefix}_${ch}"
+    python raed_write_xinum_image.py --file_name "$file_name" --path_morphology_focus "$path_morphology_focus" --scale "$scale"  --ch "$ch" --prefix "$pref"
+done
+
+file_name="morphology_focus_0003.ome.tif"
+prefix="morphology_focus_0003"
+
+for ch in "${channels[@]}"
+do
+    pref="${prefix}_${ch}"
+    python raed_write_xinum_image.py --file_name "$file_name" --path_morphology_focus "$path_morphology_focus" --scale "$scale"  --ch "$ch" --prefix "$pref"
+done
