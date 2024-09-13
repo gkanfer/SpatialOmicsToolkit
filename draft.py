18be2e915be406729c4466196c588b24102ff480
“Fulan <gkanfer007@gmail.com>
2024-09-13 11:56:41 -0400

    PCA_KNN_example

--- draft.py
+++ draft.py
@@ -1,51 +1,27 @@
+from numpy.random import default_rng
+
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-from matplotlib.colors import ListedColormap
-import matplotlib.ticker as ticker
-import matplotlib.pyplot as plt
-import pandas as pd
-from matplotlib.ticker import MaxNLocator
-import seaborn as sns
-import os
-import gzip
-import numpy as np
+
 import scanpy as sc
 import squidpy as sq
-import cupy as cp
-import cupyx
-from cupyx.scipy.sparse import csr_matrix 
-import os
-import time
-import rapids_singlecell as rsc
-import numpy as np
-import rmm
-from rmm.allocators.cupy import rmm_cupy_allocator
-import cupy
-
-rmm.reinitialize(
-    managed_memory=False,  # Allows oversubscription
-    pool_allocator=False,  # default is False
-    devices=0,  # GPU device IDs to register. By default registers only GPU 0.
-)
-cp.cuda.set_allocator(rmm_cupy_allocator)
-import zarr
-import pickle
-from collections import OrderedDict
-from scipy.sparse import csr_matrix
-import pandas as pd
-import scipy.stats as stats
-from statsmodels.stats.multitest import multipletests
-from scipy.sparse import csr_matrix
-import scipy
-import anndata
-from collections import OrderedDict
-from rsc_functions.utility.applyqc import applyqc
-from rsc_functions.reports.plot import plot_spatial,plot_spatial_data, plot_dist
-from rsc_functions.utility.rank_genes_groups import return_markers,rank_genes_groups
-from rsc_functions.reports.plot import plot_expression
 from anndata import AnnData
-counts = pd.read_csv('Rsim_count.csv')
-adata = AnnData(counts.iloc[:,1:].to_numpy(), obsm={"spatial": pd.read_csv('Rsim_centroids.csv').loc[:,['x','y']].to_numpy()})
-rsc.get.anndata_to_GPU(adata)
-rsc.pp.highly_variable_genes(adata,flavor="seurat")
-#rsc.pp.highly_variable_genes(adata,flavor="seurat_v3")
+
+
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.backends.backend_pdf import PdfPages
+import seaborn as sns
+import random
+import pandas as pd
+
+
+pathout = "/data/kanferg/Sptial_Omics/SpatialOmicsToolkit/R_simulation"
+counts = pd.read_csv(os.path.join(pathout,'Rsim_count.csv'))
+adata = AnnData(counts.iloc[:,1:].to_numpy(), obsm={"spatial": pd.read_csv(os.path.join(pathout,'Rsim_centroids.csv')).loc[:,['x','y']].to_numpy()})
+sc.pp.normalize_total(adata)
+sc.pp.log1p(adata)
+sc.pp.pca(adata, n_comps=2)
+sc.pp.neighbors(adata,n_neighbors = 10,method='gauss')
