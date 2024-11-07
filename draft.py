081792642d2e382909078fcec3bc141aafbc8df7
“Fulan <gkanfer007@gmail.com>
2024-11-07 15:30:29 -0500

    STAligner add

--- draft.py
+++ draft.py
@@ -1,13 +1,32 @@
+import os
+import tempfile
+
+import numpy as np
 import scanpy as sc
+import scvi
+import seaborn as sns
+import torch
+from rich import print
+# from scib_metrics.benchmark import Benchmarker
 
+scvi.settings.seed = 0
+torch.set_float32_matmul_precision("high")
+save_dir = tempfile.TemporaryDirectory()
 
-path_016 = "/data/kanferg/Sptial_Omics/playGround/Data/Tonsil/if/binned_outputs/square_016um"
-pathout = "/data/kanferg/Sptial_Omics/SpatialOmicsToolkit/out_3"
+pathout = "/data/kanferg/Sptial_Omics/SpatialOmicsToolkit/out_4"
+andata = sc.read_h5ad(os.path.join(pathout, "adata_concat_BreastCancer_harmony.h5ad"))
 
-andata = sc.read_visium(path=path_016)
-
-
-
+scvi.model.SCVI.setup_anndata(andata, layer="counts", batch_key="batch")
+model_scvi = scvi.model.SCVI(andata)
+max_epochs_scvi = np.min([round((20000 / andata.n_obs) * 400), 400])
+model_scvi.train()
+andata.obsm["X_scVI"] = model_scvi.get_latent_representation()
+sc.pp.neighbors(andata,n_pcs = 15, use_rep="X_scVI",key_added = 'scVI')
+sc.tl.leiden(andata, random_state=1337, resolution=0.5, key_added='cluster_scVI', neighbors_key='scVI')
+sc.tl.umap(adata_concat, neighbors_key="scVI")
+adata_concat.obsm["scVI_umap"] = adata_concat.obsm["X_umap"]
+andata_save = adata_concat.copy()
+andata_save.write_h5ad(os.path.join(pathout, "adata_concat_BreastCancer_harmony_scVI.h5ad"))
 
 
 
