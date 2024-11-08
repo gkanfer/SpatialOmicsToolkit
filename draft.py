28a2547449ed9fda8f633f84b60d0388447423a0
“Fulan <gkanfer007@gmail.com>
2024-11-08 10:23:57 -0500

    run scVI

--- draft.py
+++ draft.py
@@ -18,15 +18,24 @@ andata = sc.read_h5ad(os.path.join(pathout, "adata_concat_BreastCancer_harmony.h
 
 scvi.model.SCVI.setup_anndata(andata, layer="counts", batch_key="batch")
 model_scvi = scvi.model.SCVI(andata)
-max_epochs_scvi = np.min([round((20000 / andata.n_obs) * 400), 400])
-model_scvi.train()
+max_epochs_scvi = np.min([round((20000 / andata.n_obs) * 100), 100])
+model_scvi.train(max_epochs=27)
 andata.obsm["X_scVI"] = model_scvi.get_latent_representation()
-sc.pp.neighbors(andata,n_pcs = 15, use_rep="X_scVI",key_added = 'scVI')
-sc.tl.leiden(andata, random_state=1337, resolution=0.5, key_added='cluster_scVI', neighbors_key='scVI')
-sc.tl.umap(adata_concat, neighbors_key="scVI")
-adata_concat.obsm["scVI_umap"] = adata_concat.obsm["X_umap"]
 andata_save = adata_concat.copy()
 andata_save.write_h5ad(os.path.join(pathout, "adata_concat_BreastCancer_harmony_scVI.h5ad"))
+
+
+
+
+# andata.obsm["X_scVI"].shape
+# (293960, 10)
+#
+# sc.pp.neighbors(andata, use_rep="X_scVI",key_added = 'scVI')
+# sc.tl.leiden(andata, random_state=1337, resolution=0.5, key_added='cluster_scVI', neighbors_key='scVI')
+# sc.tl.umap(adata_concat, neighbors_key="scVI")
+# adata_concat.obsm["scVI_umap"] = adata_concat.obsm["X_umap"]
+# andata_save = adata_concat.copy()
+# andata_save.write_h5ad(os.path.join(pathout, "adata_concat_BreastCancer_harmony_scVI.h5ad"))
 
 
 
